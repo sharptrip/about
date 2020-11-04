6470d348e34af11107ba356cdd9b1f74a1003fb7
sharptrip <mlg-spbu@yandex.ru>
2020-11-04 22:21:07 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+function main() {
+  const canvas = document.querySelector('#glCanvas');
+  // Initialize the GL context
+  const gl = canvas && canvas.getContext('webgl');
+  if (!gl) {
+    console.error('Unable to initialize WebGL.');
+    return;
+  }
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+main()
