16184de4b3cc359135c0d6dd7fae0309fc3c9b90
sharptrip <mlg-spbu@yandex.ru>
2020-11-04 22:40:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -153,11 +153,6 @@ function main() {
     console.error('Unable to initialize WebGL.');
     return;
   }
-  // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  
   const shaderProgram = initShaderProgram(gl);
   // Collect all the info needed to use the shader program.
   // Look up which attribute our shader program is using
